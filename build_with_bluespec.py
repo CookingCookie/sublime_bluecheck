29b1f38ad03803e9a5bfa5a4f083d5228cdc7fcd
CookingCookie <CookingCookie@gmx.de>
2018-02-13 12:53:51 +0100

    Commit

--- build_with_bluespec.py
+++ build_with_bluespec.py
@@ -9,7 +9,7 @@ class BuildWithBluespecCommand(sublime_plugin.TextCommand):
       tu_id = tu_id_file.readline()
       tu_id_file.close()
       tmp_file = open(file_path+'tmp', 'w+')
-      tmp_file.write('#!/bin/bash\nBSV_FILE_PATH=$1\nBSV_FILE_NAME=$2\nBSV_MODULE=$3\nrsync $BSV_FILE_PATH '+tu_id+'@clientssh2.rbg.informatik.tu-darmstadt.de:~/AER/tmp/ > /dev/null 2>&1 && ssh -t kg50hysu@clientssh2.rbg.informatik.tu-darmstadt.de "export BLUESPECDIR=/opt/bluespec/lib ; export LM_LICENSE_FILE=27002@licence.rbg.informatik.tu-darmstadt.de ; cd ~/AER/tmp/ ; cp ~/AER/BlueCheck.bsv BlueCheck.bsv ; cp ~/AER/BlueCheck.bo BlueCheck.bo ; /opt/bluespec/bin/bsc -u -sim -g $BSV_MODULE $BSV_FILE_NAME ; /opt/bluespec/bin/bsc -sim -o out -e $BSV_MODULE ; echo "----------Start Simulation----------" ; ./out ; echo "-----------End Simulation-----------" ; rm -r ~/AER/tmp/*"')     
+      tmp_file.write('#!/bin/bash\nBSV_FILE_PATH=$1\nBSV_FILE_NAME=$2\nBSV_MODULE=$3\nrsync $BSV_FILE_PATH '+tu_id+'@clientssh2.rbg.informatik.tu-darmstadt.de:~/AER/tmp/ > /dev/null 2>&1 && ssh -t '+tu_id+'@clientssh2.rbg.informatik.tu-darmstadt.de "export BLUESPECDIR=/opt/bluespec/lib ; export LM_LICENSE_FILE=27002@licence.rbg.informatik.tu-darmstadt.de ; cd ~/AER/tmp/ ; cp ~/AER/BlueCheck.bsv BlueCheck.bsv ; cp ~/AER/BlueCheck.bo BlueCheck.bo ; /opt/bluespec/bin/bsc -u -sim -g $BSV_MODULE $BSV_FILE_NAME ; /opt/bluespec/bin/bsc -sim -o out -e $BSV_MODULE ; echo "----------Start Simulation----------" ; ./out ; echo "-----------End Simulation-----------" ; rm -r ~/AER/tmp/*"')     
       tmp_file.close()
       st = os.stat(file_path+'tmp')
       os.chmod(file_path+'tmp', st.st_mode | stat.S_IEXEC)
